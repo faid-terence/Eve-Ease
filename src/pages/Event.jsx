bdd32fb4b16465020d81edafc7be4f411df8e2f9
Terence Faid JABO <faidterence@outlook.com>
2024-01-07 00:22:18 +0200

    feat(events):Search Form implemented

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export const Event = () => {
   return (
-    <div>Event</div>
-  )
-}
+    <section>
+      <div className="container text-center">
+        <h2 className="heading">Find Events</h2>
+        <div className="max-w-[570px] mt-[30px] mx-auto bg-[#CCF4B3] rounded-md flex items-center justify-between">
+          <input
+            type="search"
+            className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
+            placeholder="Search a Doctor"
+          />
+          <button className="btn mt-0 rounded-[0px] rounded-r-md">
+            Search
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
