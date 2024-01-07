88078bb6a3344bb20faf474cbf5c58b5200dc626
Terence Faid JABO <faidterence@outlook.com>
2024-01-08 00:30:50 +0200

    feat(events):Search form updated

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -15,7 +15,7 @@ export const Event = () => {
             <input
               type="search"
               className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
-              placeholder="Search a Doctor"
+              placeholder="Search for an event"
             />
             <button className="btn mt-0 rounded-[0px] rounded-r-md">
               Search
