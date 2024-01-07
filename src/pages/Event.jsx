f39734a81868ad9398c49780e5823a25a45b4247
Terence Faid JABO <faidterence@outlook.com>
2024-01-07 21:41:14 +0200

    feat(event):Section responsiveness

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { EventCard } from "../components/Event/Card";
 import eventImage from "../assets/images/img1.jpg";
+import eventImageII from "../assets/images/img2.jpg";
+import eventImageIII from "../assets/images/img3.jpg";
+import eventImageIV from "../assets/images/img4.jpg";
 
 export const Event = () => {
   return (
@@ -22,65 +25,68 @@ export const Event = () => {
       </section>
       <section>
         <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="col-span-full">
+            <h2 className="heading pl-8">Upcoming events : </h2>
+          </div>
           <EventCard
             eventImage={eventImage}
             title="Bubbe's Book Club"
             location="Bellmore, NY"
             price="Free"
             venue="Grand Central Terminal"
-            dateTime="Sat, Sep 19, 10:00 AM EDT"
+            dateTime="Sat, Sep 24, 10:00 AM EDT"
             description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
           />
           <EventCard
-            eventImage={eventImage}
-            title="Bubbe's Book Club"
+            eventImage={eventImageII}
+            title="The Overstory"
+            location="New York, NY"
+            price="29$"
+            venue="245 W 52nd St, New York"
+            dateTime="Sat, Sep 24, 10:00 AM EDT"
+            description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
+          />
+          <EventCard
+            eventImage={eventImageIII}
+            title="The NY Festival"
             location="Bellmore, NY"
-            price="Free"
+            price="70$"
+            venue="Grand Central Terminal"
+            dateTime="Sat, Sep 24, 10:00 AM EDT"
+            description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
+          />
+          <EventCard
+            eventImage={eventImageIV}
+            title="Tech Bubble Conf"
+            location="Bellmore, NY"
+            price="65$"
             venue="Grand Central Terminal"
             dateTime="Sat, Sep 19, 10:00 AM EDT"
             description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
           />
           <EventCard
-            eventImage={eventImage}
-            title="Bubbe's Book Club"
+            eventImage={eventImageIV}
+            title="Tech Bubble Conf"
             location="Bellmore, NY"
-            price="Free"
+            price="65$"
             venue="Grand Central Terminal"
             dateTime="Sat, Sep 19, 10:00 AM EDT"
             description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
           />
           <EventCard
-            eventImage={eventImage}
-            title="Bubbe's Book Club"
+            eventImage={eventImageIII}
+            title="The NY Festival"
             location="Bellmore, NY"
-            price="Free"
+            price="70$"
             venue="Grand Central Terminal"
             dateTime="Sat, Sep 19, 10:00 AM EDT"
             description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
           />
           <EventCard
-            eventImage={eventImage}
-            title="Bubbe's Book Club"
+            eventImage={eventImageII}
+            title="The Overstory"
             location="Bellmore, NY"
-            price="Free"
-            venue="Grand Central Terminal"
-            dateTime="Sat, Sep 19, 10:00 AM EDT"
-            description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
-          />
-          <EventCard
-            eventImage={eventImage}
-            title="Bubbe's Book Club"
-            location="Bellmore, NY"
-            price="Free"
-            venue="Grand Central Terminal"
-            dateTime="Sat, Sep 19, 10:00 AM EDT"
-            description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
-          />
-          <EventCard
-            eventImage={eventImage}
-            title="Bubbe's Book Club"
-            location="Bellmore, NY"
-            price="Free"
+            price="39$"
             venue="Grand Central Terminal"
             dateTime="Sat, Sep 19, 10:00 AM EDT"
             description="Welcome! Everyone has a unique perspective after reading a book, and we would love you to share yours with us! We meet one Sunday evening"
