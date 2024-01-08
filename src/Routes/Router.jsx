5cb5e1fed50209a2fa1279d3fe95ed827a1fbadf
Terence Faid JABO <faidterence@outlook.com>
2024-01-08 17:27:24 +0200

    feat(login):Login Page Implemented

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -5,6 +5,7 @@ import { Event } from "../pages/Event";
 import { Tickets } from "../pages/Tickets";
 import { Contact } from "../pages/Contact";
 import { SignUp } from "../pages/SignUp";
+import { Login } from "../pages/Login";
 
 export const Router = () => {
   return (
@@ -14,6 +15,7 @@ export const Router = () => {
       <Route path="/events" element={<Event />} />
       <Route path="/tickets" element={<Tickets />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/auth/signin" element={<Login />} />
       <Route path="/auth/register" element={<SignUp />} />
     </Routes>
   );
