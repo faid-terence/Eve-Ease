1eb62f5dfb6caa2af50008864cc27b5ce1db40e7
Terence Faid JABO <faidterence@outlook.com>
2024-01-19 00:05:23 +0200

    Add responsive hero section with carousel

--- src/components/CreateEventForm.jsx
+++ src/components/CreateEventForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import profile from "../assets/terence 1.png";
 
 export const CreateEventForm = () => {
   return (
@@ -110,5 +111,77 @@ export const CreateEventForm = () => {
         </form>
       </div>
     </section>
+
+    // <section>
+    //   <div className="container">
+    //     <div className="relative mt-10 mb-6 shadow-lg w-[90%]">
+    //       <p className="font-medium mb-2 inline-block">Admin User Management</p>
+    //       <div className="w-full pb-2 overflow-x-auto">
+    //         <table className="font-light w-full">
+    //           <thead className="bg-blue-200">
+    //             <tr>
+    //               <th className="w-32 text-start font-bold px-6 py-1 capitalize">
+    //                 Account Status
+    //               </th>
+    //               <th className="w-32 text-start font-bold px-6 py-1 capitalize">
+    //                 User Name
+    //               </th>
+    //               <th className="w-32 text-start font-bold px-6 py-1 capitalize">
+    //                 User Email
+    //               </th>
+    //               <th className="w-32 text-start font-bold px-6 py-1">
+    //                 User Role
+    //               </th>
+    //               <th className="w-32 text-start font-bold px-6 py-1">
+    //                 Action
+    //               </th>
+    //             </tr>
+    //           </thead>
+    //           <tbody>
+    //             <tr>
+    //               <td className="w-32 text-start px-6 py-1">
+    //                 <button className="bg-customBlue hover:bg-customBlue text-white font-bold py-1 px-6 rounded-lg capitalize">
+    //                   Active
+    //                 </button>
+    //               </td>
+
+    //               <td className="w-48 font-light py-4 rounded-lg flex items-center">
+    //                 <img
+    //                   src={profile}
+    //                   alt="User"
+    //                   className="w-8 h-8 mr-2 rounded-full"
+    //                 />
+    //                 John Doe{" "}
+    //               </td>
+
+    //               <td className="w-48 text-start font-light px-6 py-1">
+    //                 john.doe@example.com{" "}
+    //               </td>
+    //               <td className="w-48 text-start font-light px-6 py-1">
+    //                 Admin
+    //               </td>
+    //               <td className="w-32 text-start font-light px-6 py-1">
+    //                 <div className="symbols flex justify-between">
+    //                   <i className="material-symbols-rounded cursor-pointer text-customBlue-500">
+    //                     visibility
+    //                   </i>
+    //                   <i className="update material-symbols-rounded cursor-pointer text-orange-500">
+    //                     edit
+    //                   </i>
+    //                   <i className="material-symbols-rounded cursor-pointer text-red-500">
+    //                     delete_forever
+    //                   </i>
+    //                   <i className="material-symbols-rounded cursor-pointer">
+    //                     manage_accounts
+    //                   </i>
+    //                 </div>
+    //               </td>
+    //             </tr>
+    //           </tbody>
+    //         </table>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
   );
 };
